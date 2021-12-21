c9c3e424e224a026c639fbf012f9ec019a7c9f03
장 세영(Jang Seyoung) <92082361+hotsauceuniverse@users.noreply.github.com>
2021-12-21 15:54:48 +0900

    Add files via upload

---
+++ src/components/MovieList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="inner">
+      <MovieItem  
+        v-for="movie in movies"  
+        :key="movie.imdbID"
+        :movie="movie" /> 
+    </div>
+  </div>
+  <!-- v-for의 movies는 computed의 movies(배열 데이터) -->
+  <!-- for문의 movies라는 배열데이터 안에서 각각의 객체 아이템을 v-for라는 디렉티브로 반복해서 movie라는 이름으로 사용하는 것이고,
+   그 movie안에 imdbID라는 속성의 문자데이터로 반복되는 아이템의 고유값을 정의해 주고, 
+   그 객체데이터 자체인 movie라는 데이터를 movie라는 이름으로 MovieItem이라는 컴포넌트에 전달해 준다. -->
+</template>
+
+<script>
+import MovieItem from './MovieItem.vue'
+export default {
+  computed: {
+    movies() {
+      return this.$store.state.movie.movies
+    }
+  },
+
+  components: {
+    MovieItem
+  }
+}
+</script>
---
+++ src/components/Search.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="container">
+    <input 
+      v-model="title" 
+      class="form-control"
+      type="text"
+      placeholder="Search for Movies, Series & more" 
+      @keyup.enter="apply"/>
+      <div class="selects">
+          <select 
+          v-for="filter in filters" 
+          v-model="$data[filter.name]"  
+          :key="filter.name" 
+          class="form-select">
+          <option v-if="filter.name === 'year'" value="">
+            All Years <!-- All Years가 전체 drop down에 적용되기 때문에 v-if적용 -->
+          </option>
+          <option
+            v-for="item in filter.items"
+            :key="item">
+            {{ item }}
+          </option>
+        </select>
+      </div>
+      <button 
+        class="btn btn-primary"
+        @click="apply">
+        Apply
+      </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+      type: 'movie',
+      number: 10,
+      year: '',
+      filters: [
+        {
+          name: 'type',
+          items: ['movie', 'series', 'episode']
+        },
+        {
+          name: 'number',
+          items:[10, 20, 30]
+        },
+        {
+          name: 'year',
+          items: (() => {
+            const years =[]
+            const thisYear = new Date().getFullYear()
+            for (let i = thisYear; i >= 1985; i -= 1) {
+              years.push(i)
+            }
+            return years
+          })() // 즉시실행 함수
+        }
+      ]
+    }
+  },
+  methods: {
+    async apply() { // 비동기 동작 = async
+      this.$store.dispatch('movie/searchMovies', {
+        title: this.title,
+        type: this.type,
+        number: this.number,
+        year: this.year,
+      })
+    }
+  } // Store의 Mutation를 실행할 때는 .commit()메소드를, Action를 실행할 때는 .dispatch() 메소드를 사용한다.
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  > * {
+    margin-right: 10px;
+    font-size: 15px;
+    &:last-child {
+      margin-right: 0;
+    }
+  } 
+  .selects{
+    display: flex;
+    select {
+    width: 120px;
+    margin-right: 10px;
+    &:last-child {
+      margin-right: 0;
+      }
+    }
+  }
+  .btn {
+    width: 120px;
+    height: 50px;
+    font-weight: 700;
+    flex-shrink: 0; /* 감소가 될 수 있는 비율이 없도록 만들어줘야됨 */
+  }
+}
+</style>
