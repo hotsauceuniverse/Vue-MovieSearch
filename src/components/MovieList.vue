9a2796452182c8fd1a91cee36994bcfc087bfae9
장 세영(Jang Seyoung) <92082361+hotsauceuniverse@users.noreply.github.com>
2021-12-27 23:30:27 +0900

    Update MovieList.vue

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="container">
-    <div class="inner">
-      <MovieItem  
+    <div 
+      :class="{ 'no-result': !movies.length }" 
+      class="inner">
+      <div 
+        v-if="loading" 
+        class="spinner-border text-primary"></div>
+      <div v-if="message" class="message">
+        {{ message }}
+      </div>
+      <div v-else class="movies">
+        <MovieItem  
         v-for="movie in movies"  
         :key="movie.imdbID"
-        :movie="movie" /> 
+        :movie="movie" /></div> 
     </div>
   </div>
   <!-- v-for의 movies는 computed의 movies(배열 데이터) -->
@@ -15,10 +24,17 @@
 
 <script>
 import MovieItem from './MovieItem.vue'
+
 export default {
   computed: {
     movies() {
       return this.$store.state.movie.movies
+    },
+    message() {
+      return this.$store.state.movie.message
+    },
+    loading() {
+      return this.$store.state.movie.loading
     }
   },
 
@@ -27,3 +43,29 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import "../scss/main.scss";
+
+.container {
+  margin-top: 30px;
+  .inner {
+    background-color: $gray-200;
+    padding: 10px 0;
+    border-radius: 4px;
+    text-align: center;
+    &.no-result {
+      padding: 70px 0;
+    }
+  }
+  .message {
+    color: $gray-400;
+    font-size : 20px;
+  }
+  .movies {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+}
+</style>
