0b1cae1eac3f463cb2173c1b90b63b6b281551cd
장 세영(Jang Seyoung) <92082361+hotsauceuniverse@users.noreply.github.com>
2021-12-28 17:00:05 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes/index.js'
 import store from './store/index.js'
+import loadImage from './plugins/loadImage.js'
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App)
+  .use(router) // $route, $router
+  .use(store) // $store
+  .use(loadImage) // $loadImage
+  .mount('#app')
 
 // use라는 메소드는 vue에 연결할 플러그인 / 라이브러리들을 지정하는 용도
