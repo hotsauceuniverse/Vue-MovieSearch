bf6eb229e804319aa78956f3681f10bb598599ed
장 세영(Jang Seyoung) <92082361+hotsauceuniverse@users.noreply.github.com>
2021-12-30 17:38:47 +0900

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,14 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import Home from './Home.vue'
 import Movie from './Movie.vue'
 import About from './About.vue'
+import NotFound from './NotFound.vue'
 
 export default createRouter({
   // Hash mode - 특정페이지에서 새로고침을 했을 때, 페이지를 찾을 수 없다는 메세지를 방지 할 수 있다.
   history: createWebHashHistory(),
+  scrollBehavior () {
+    return{ top: 0 }
+  },
   // pages
   routes: [
     {
@@ -19,6 +23,10 @@ export default createRouter({
     {
       path:'/about', 
       component: About
+    },
+    {
+      path: '/:notFound(.*)',
+      component: NotFound
     }
   ]
 })
